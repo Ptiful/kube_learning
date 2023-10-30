25b445f32f1b6281c91c6f143c2b2379ca46bed0
Ptiful <strazzu@gmail.com>
2023-10-30 16:11:05 +0100

    modified dags

--- dags/dag.py
+++ dags/dag.py
@@ -1,8 +1,29 @@
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.docker_operator import DockerOperator
 
-with DAG("dag", 
-        start_date=datetime(2023,10,10), 
-        schedule_interval="@daily", catchup=False) as dag:
-        
+# kubernator operator instead of docker operator
+# git sync in values.yaml
+# sync repo
+default_args = {
+    "owner": "paul",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 10, 27),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "hln_scraper",
+    default_args=default_args,
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    task_1 = DockerOperator(
+        task_id="hln_scraper",
+        image="paulstrazzulla/hln_scraper:latest",
+    )
